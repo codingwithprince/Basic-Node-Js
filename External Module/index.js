8af8d9cd36f136a37c12f44bb0b923e5cd7afedb
Prince Ahmed <codingwithprince@gmail.com>
2021-03-31 15:11:45 +0600

    added external module

---
+++ External Module/index.js
@@ -0,0 +1,9 @@
+const randomFruitsName = require('random-fruits-name');
+const movieName = require('movies-names');
+
+
+console.log(randomFruitsName('en'));
+
+const mname = movieName.random();
+
+console.log(mname);
